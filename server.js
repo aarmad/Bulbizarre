d95d8ccecc96560a1382f52b00aa8b0885bca3cb
Guillaume Marande <gmarande@outlook.fr>
2026-04-16 11:25:25 +0200

    test

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ Connexion à MongoDB Atlas réussie !");
+    console.log("Connexion à MongoDB Atlas réussie !");
   } catch (err) {
-    console.error("❌ Erreur de connexion :", err.message);
+    console.error("Erreur de connexion :", err.message);
     process.exit(1);
   }
 };
